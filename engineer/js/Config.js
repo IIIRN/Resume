e335564451ac2c390f0b40cc66bb772e7297ce99
IIIRN (3RN) Studio <95156967+IIIRN@users.noreply.github.com>
2026-02-27 20:15:11 +0700

    Add files via upload

---
+++ engineer/js/Config.js
@@ -0,0 +1,13 @@
+
+window.APP_CONFIG = {
+    แบรนด์: {
+        ชื่อ: "POWERTEC ENGINEERING",
+    },
+    LIFF: {
+        LIFF_ID: "2005492705-4943000000",
+    },
+    API: {
+        GAS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbwZ_JfMBnPdBeVz4RGsw0tPkb4V7WkyTl1KqhOC2SFtVjYrEnQcSjT6UWrgKzB3GUX-1A/exec",
+        API_KEY: "powertec_2026_SECURE_9fA3Kx81" // ต้องตรงกับ Code.gs
+    }
+};
